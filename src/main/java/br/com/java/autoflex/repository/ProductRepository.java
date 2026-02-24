085fa0fefb39e98e7db8633b1c2eebc15c11e641
BrunodaSilvaMachado <brunosilvamachado@id.uff.br>
2026-02-24 17:32:03 -0300

    prevent N+1 problem

--- src/main/java/br/com/java/autoflex/repository/ProductRepository.java
+++ src/main/java/br/com/java/autoflex/repository/ProductRepository.java
@@ -2,11 +2,21 @@ package br.com.java.autoflex.repository;
 
 import br.com.java.autoflex.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByCode(String code);
 
     boolean existsByCode(String code);
+
+    @Query("""
+        SELECT p FROM Product p
+        LEFT JOIN FETCH p.materials pm
+        LEFT JOIN FETCH pm.rawMaterial
+            """)
+    List<Product> findAllWithMaterials();
     
 }
